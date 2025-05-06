1ff6678faad25436f69d948f2d49c465c7115859
goldendevuz <goldendevuz@gmail.com>
2025-05-06 10:21:39 +0500

    2025.05.06/10:21

--- core/settings.py
+++ core/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django_admin_index",
-    "ordered_model",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
